43b362b4cb6c9bfddfd8f576e186da7d73f7b031
legendaryMisco <91750453+legendaryMisco@users.noreply.github.com>
2023-05-28 16:56:17 +0100

    Update settings.py

--- MEproject/settings.py
+++ MEproject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,17 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-environ.Env.read_env()
-
-
-
-
-SECRET_KEY = env('SECRET_KEY')
-DEBUG = env('DEBUG')
+SECRET_KEY = 'django-insecure-9pw2%#zuti6vw4xpmfdmuoo=osba(@r9cs7w-q)3#74fxx6hhx'
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -134,7 +124,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'carolynarthur788@gmail.com'
+EMAIL_HOST_PASSWORD = 'django-insecure-9pw2%#zuti6vw4xpmfdmuoo=osba(@r9cs7w-q)3#74fxx6hhx'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
